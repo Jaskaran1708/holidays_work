5a44d5927849a7c257a346badfcf2b4f2db7b8a1
Jaskaran1708 <82132695+Jaskaran1708@users.noreply.github.com>
2022-07-02 00:48:02 +0530

    Update disappearing no. in array

--- numbers_disappeared_in array.py
+++ numbers_disappeared_in array.py
@@ -1,17 +1,25 @@
-a=[2,3,4,5,7]
+def search(arr, low, high, x):
+    if high >= low:
+ 
+        mid = (high + low) // 2
+        if arr[mid] == x:
+            return True
+        elif arr[mid] > x:
+            return search(arr, low, mid - 1, x)
+        else:
+            return search(arr, mid + 1, high, x)
+    else:
+        return False
+a = [2,3,4,6,5]
+a.sort()
 n = len(a)
 b = []
 c=[]
 for i in range(1,n+1):
     b.append(i)
-for f in range(0,n):
-    for j in range(0,n):
-        if a[f] != b[j]:
-            print(b)
-            # c.append(a[f])
-# for r in range(0,n):
-#     for e in range(0,n):
-#         if b[r] != c[e]:
-#             print(b[r])
-
-
+for i in b:
+    if search(a,0,n-1,i):
+        continue
+    else:
+        c.append(i)
+print(c)
