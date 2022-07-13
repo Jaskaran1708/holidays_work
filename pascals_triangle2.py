e8f9c3621ec075b40210f28129934519587ce090
Jaskaran1708 <82132695+Jaskaran1708@users.noreply.github.com>
2022-07-14 00:37:13 +0530

    Pascal's triangle 2

---
+++ pascals_triangle2.py
@@ -0,0 +1,16 @@
+from math import factorial
+def c(a,b):
+    return int(factorial(a)/(factorial(b)*factorial(a-b)))
+a = [1]
+b = [a]
+a = [1,1]
+n=5
+b.append(a)
+if n ==1:
+    print(b[1])
+for i in range(1,n+1):
+    a=[]
+    for j in range(0,len(b[i])+1):
+        a.append(c(len(b[i]),j))
+    b.append(a)
+print(b[n])
